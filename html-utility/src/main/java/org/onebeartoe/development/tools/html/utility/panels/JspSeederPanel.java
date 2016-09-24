b2da0ae75cec17481e72fef441172050b4d35b22
onebeartoe <onebeartoe@gmail.com>
2016-09-24 09:53:42 -0500

    This is the start of a JSP template creator.

---
+++ html-utility/src/main/java/org/onebeartoe/development/tools/html/utility/panels/JspSeederPanel.java
@@ -0,0 +1,78 @@
+/*
+
+ */
+package org.onebeartoe.development.tools.html.utility.panels;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.border.Border;
+
+/**
+ *
+ * @author Roberto Marquez <https://www.youtube.com/user/onebeartoe>
+ */
+public class JspSeederPanel extends JPanel implements ActionListener
+{
+
+    private final FileSelectionPanel fileSelectionPanel;
+
+    private final JButton actionButton;
+
+    private final ScrollableTextArea statusPanel;
+    
+    public JspSeederPanel()
+    {
+        // this panel gives the user a button to click to pick an input direcotyr,  it also shows which files will be worked on.
+        boolean showRecursive = true;
+        fileSelectionPanel = new FileSelectionPanel(FileType.IMAGE, FileSelectionMethods.SINGLE_DIRECTORY, showRecursive);
+        Border border = GUITools.factoryLineBorder("Input Files");
+        fileSelectionPanel.setBorder(border);
+        
+        JPanel inputPanel = new JPanel(new BorderLayout());
+        inputPanel.add(fileSelectionPanel, BorderLayout.CENTER);
+
+        // this panel holds the  buttons that start the thumbnail generation and displays the status of the application.  Tthis is the text area the displays the status of the application
+        statusPanel = new ScrollableTextArea("\n\n");
+
+        // this panel holds the  buttons that start the thumbnail generation				
+        actionButton = new JButton("Resize");
+        actionButton.addActionListener(this);
+
+        // place the status and action components onto a panel
+        JPanel controlPanel = new JPanel();
+        controlPanel.setLayout(new BorderLayout());
+        controlPanel.add(statusPanel, BorderLayout.CENTER);
+        controlPanel.add(actionButton, BorderLayout.SOUTH);
+        Border border3 = GUITools.factoryLineBorder("Control and Status");
+        controlPanel.setBorder(border3);
+        controlPanel.setMinimumSize( new Dimension(300, 1) );
+
+        // define the JFrame content layout        
+        setLayout(new BorderLayout());
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, inputPanel, controlPanel);
+        splitPane.setOneTouchExpandable(true);
+        add(splitPane, BorderLayout.CENTER);
+    }
+
+    public void actionPerformed(ActionEvent ae) 
+    {
+        Object eventSource = ae.getSource();
+        if (eventSource == actionButton) 
+        {            
+            File sourceDirectory = fileSelectionPanel.getCurrentDirectoty();     
+            TimerTask task = new IndexTask(sourceDirectory, statusPanel);
+            Date date = new Date();
+            Timer timer = new Timer();
+            timer.schedule(task, date);           
+        }
+    }    
+}
---
+++ html-utility/src/main/java/org/onebeartoe/development/tools/html/utility/tasks/JspSeederTask.java
@@ -0,0 +1,18 @@
+
+package org.onebeartoe.development.tools.html.utility.tasks;
+
+import java.util.TimerTask;
+
+/**
+ *
+ * Given the filesystem location of a JEE Web directory and an accessible (target) 
+ * directoy, create supporting index.jsp, WEB-INF/jsp/.../bottom.jsp, 
+ * WEB-INF/jsp/.../index.jsp, and properties.jsp
+ * 
+ * @author Roberto Marquez <https://www.youtube.com/user/onebeartoe>
+ * 
+ */
+public class JspSeederTask extends TimerTask
+{
+    
+}
