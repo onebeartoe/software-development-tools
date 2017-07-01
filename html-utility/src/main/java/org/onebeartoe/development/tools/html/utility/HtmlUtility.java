c6a81fe14ba974e0585cdfe0a5b8ed1d6ae847d0
Roberto <URHM020@L7TX8B0102409.int.carlsonwagonlit.com>
2017-07-01 08:59:27 -0500

    The window properties are now saved between runs.

--- html-utility/src/main/java/org/onebeartoe/development/tools/html/utility/HtmlUtility.java
+++ html-utility/src/main/java/org/onebeartoe/development/tools/html/utility/HtmlUtility.java
@@ -1,8 +1,14 @@
 
 package org.onebeartoe.development.tools.html.utility;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
-
+import org.onebeartoe.application.DesktopApplication;
+import org.onebeartoe.application.ui.WindowProperties;
 import org.onebeartoe.application.ui.swing.TabbedPane;
 import org.onebeartoe.development.tools.html.utility.panels.JspSeederPanel;
 
@@ -11,9 +17,11 @@ import org.onebeartoe.development.tools.html.utility.panels.JspSeederPanel;
  * @author Roberto Marquez
  * 
  */
-public class HtmlUtility extends JFrame
+public class HtmlUtility extends JFrame implements DesktopApplication
 {
-    TabbedPane tabbedPane;
+    private TabbedPane tabbedPane;
+    
+    private static WindowProperties wp;
 
     public HtmlUtility() 
     {
@@ -30,14 +38,57 @@ public class HtmlUtility extends JFrame
         tabbedPane.setTabTitles(titles);
         
         getContentPane().add( tabbedPane );
-        setSize(800, 500);
-        setLocation(215,100);
+        
+        String applicationName = getClass().getName();
+        try 
+        {
+            wp = restoreWindowProperties(applicationName);
+            
+            setSize(wp.width, wp.height);
+            setLocation(wp.locationX, wp.locationY);
+        } 
+        catch (IOException | ClassNotFoundException ex  ) 
+        {
+            Logger.getLogger(HtmlUtility.class.getName()).log(Level.SEVERE, null, ex);
+
+            wp = new WindowProperties();
+            wp.id = applicationName;
+            
+            // resort to default window property values
+            setSize(800, 500);
+            setLocation(215,100);
+        }
+        
+        String appName = getClass().getSimpleName();
+        wp.applicationName = appName;
+        
         setVisible(true);
     }
 
     public static void main(String [] args) 
     {
-        HtmlUtility app = new HtmlUtility();
+        final HtmlUtility app = new HtmlUtility();
+        app.addWindowListener( new WindowAdapter() 
+        {
+            @Override
+            public void windowClosing(WindowEvent we)
+            {
+                try 
+                {
+                    wp.locationX = (int) app.getLocation().getX();
+                    wp.locationY = (int) app.getLocation().getY();
+                    
+                    wp.width = app.getWidth();
+                    wp.height = app.getHeight();
+                    
+                    app.persistWindowProperties(wp);
+                } 
+                catch (IOException ex) 
+                {
+                    ex.printStackTrace();
+                }
+            }
+        });
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
     }
 }
