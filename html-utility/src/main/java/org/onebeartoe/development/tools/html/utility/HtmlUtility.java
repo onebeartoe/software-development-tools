7593e4f6d92b16235ae0a34e47ef0ab6ff554f0c
Roberto <URHM020@L7TX8B0102409.int.carlsonwagonlit.com>
2017-07-20 17:24:14 -0500

    This application now uses the updated window properties managment.

--- html-utility/src/main/java/org/onebeartoe/development/tools/html/utility/HtmlUtility.java
+++ html-utility/src/main/java/org/onebeartoe/development/tools/html/utility/HtmlUtility.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
 import javax.swing.JFrame;
 import org.onebeartoe.application.DesktopApplication;
 import org.onebeartoe.application.ui.WindowProperties;
+import org.onebeartoe.application.ui.swing.SwingApplication;
 import org.onebeartoe.application.ui.swing.TabbedPane;
 import org.onebeartoe.development.tools.html.utility.panels.JspSeederPanel;
 
@@ -17,12 +18,16 @@ import org.onebeartoe.development.tools.html.utility.panels.JspSeederPanel;
  * @author Roberto Marquez
  * 
  */
-public class HtmlUtility extends JFrame implements DesktopApplication
+public class HtmlUtility extends JFrame
 {
     private TabbedPane tabbedPane;
     
     private static WindowProperties wp;
+    
+    private SwingApplication guiConfig;
 
+    private final String applicationId;
+    
     public HtmlUtility() 
     {
         super( "HtmlUtility by onebeartoe.com" );
@@ -32,17 +37,54 @@ public class HtmlUtility extends JFrame implements DesktopApplication
         
         JspSeederPanel jsp = new JspSeederPanel();
 
-        tabbedPane = new TabbedPane(itp, ip, jsp);
+        tabbedPane = new TabbedPane(jsp, itp, ip);
 
-        String [] titles = {"Image Tagging", "Directory Listing", "JSP Seeder"};
+        String [] titles = {"JSP Seeder", "Image Tagging", "Directory Listing"};
         tabbedPane.setTabTitles(titles);
         
         getContentPane().add( tabbedPane );
         
-        String applicationName = getClass().getName();
+        applicationId = getClass().getName();
+        
+        guiConfig = new SwingApplication();
+        
+        restoreGui();
+
+        addWindowListener( new WindowAdapter() 
+        {
+            @Override
+            public void windowClosing(WindowEvent we)
+            {
+                try 
+                {                    
+                    wp = guiConfig.currentConfiguration(HtmlUtility.this);
+                    wp.id = applicationId;
+                    guiConfig.persistWindowProperties(wp);
+                } 
+                catch (IOException ex) 
+                {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        
+        setVisible(true);
+    }
+
+    public static void main(String [] args) 
+    {
+        final HtmlUtility app = new HtmlUtility();
+//        app.;
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+    }
+    
+    public void restoreGui()
+    {
         try 
         {
-            wp = restoreWindowProperties(applicationName);
+            wp = new WindowProperties();
+            wp.id = applicationId;
+            wp = guiConfig.loadWindowProperties(wp);
             
             setSize(wp.width, wp.height);
             setLocation(wp.locationX, wp.locationY);
@@ -52,43 +94,11 @@ public class HtmlUtility extends JFrame implements DesktopApplication
             Logger.getLogger(HtmlUtility.class.getName()).log(Level.SEVERE, null, ex);
 
             wp = new WindowProperties();
-            wp.id = applicationName;
+            wp.id = applicationId;
             
             // resort to default window property values
             setSize(800, 500);
             setLocation(215,100);
         }
-        
-        String appName = getClass().getSimpleName();
-        wp.applicationName = appName;
-        
-        setVisible(true);
-    }
-
-    public static void main(String [] args) 
-    {
-        final HtmlUtility app = new HtmlUtility();
-        app.addWindowListener( new WindowAdapter() 
-        {
-            @Override
-            public void windowClosing(WindowEvent we)
-            {
-                try 
-                {
-                    wp.locationX = (int) app.getLocation().getX();
-                    wp.locationY = (int) app.getLocation().getY();
-                    
-                    wp.width = app.getWidth();
-                    wp.height = app.getHeight();
-                    
-                    app.persistWindowProperties(wp);
-                } 
-                catch (IOException ex) 
-                {
-                    ex.printStackTrace();
-                }
-            }
-        });
-        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
     }
 }
