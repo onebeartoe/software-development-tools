80cb2dc9e480b9231f16c2c6a9f5a19107144573
Roberto Marquez <onebeartoe@gmail.com>
2018-09-14 15:23:23 -0500

    The test was updated with the new repository.

--- version-control/src/test/java/org/onebeartoe/tools/versioning/subversion/SubversionServiceTest.java
+++ version-control/src/test/java/org/onebeartoe/tools/versioning/subversion/SubversionServiceTest.java
@@ -25,7 +25,8 @@ public class SubversionServiceTest
         String creationDate = subversionService.creationDate(repositoryPath);
 //System.out.println("cd: " + creationDate);
 
-        String expectedDate = "2018-09-14 10:40:22 -0500 (Fri, 14 Sep 2018)";
+        String expectedDate = "2018-09-14 15:40:22 +0000 (Fri, 14 Sep 2018)";
+//        String expectedDate = "2018-09-14 10:40:22 -0500 (Fri, 14 Sep 2018)";
 //        String expectedDate = "2006-03-01 09:52:04 -0600 (Wed, 01 Mar 2006)";
         
         assert expectedDate.equals(creationDate);
