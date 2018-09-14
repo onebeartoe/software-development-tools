9e5f0dcf4314ac12c1075267765e8f243a209fdb
U-HARLANDCLARKE\A061921 <A061921@SATD-L-PC03X0U2.harlandclarke.local>
2018-09-14 10:53:03 -0500

    The Subversion URL was updated.

--- version-control/src/test/java/org/onebeartoe/tools/versioning/subversion/SubversionServiceTest.java
+++ version-control/src/test/java/org/onebeartoe/tools/versioning/subversion/SubversionServiceTest.java
@@ -19,11 +19,14 @@ public class SubversionServiceTest
     @Test
     public void testKnownDate() throws Exception
     {
-        final String repositoryPath = "http://svn.effbot.org/public/elementtree/";
+//        final String repositoryPath = "http://svn.effbot.org/public/elementtree/";
+        final String repositoryPath = "https://svn.riouxsvn.com/junit-target/";
         
         String creationDate = subversionService.creationDate(repositoryPath);
-        
-        String expectedDate = "2006-03-01 09:52:04 -0600 (Wed, 01 Mar 2006)";
+//System.out.println("cd: " + creationDate);
+
+        String expectedDate = "2018-09-14 10:40:22 -0500 (Fri, 14 Sep 2018)";
+//        String expectedDate = "2006-03-01 09:52:04 -0600 (Wed, 01 Mar 2006)";
         
         assert expectedDate.equals(creationDate);
     }
