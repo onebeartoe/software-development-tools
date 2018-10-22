c1e2dd37a188ebacc019706f1c91ad36e612db71
Roberto Marquez <onebeartoe@gmail.com>
2018-10-22 12:39:20 -0500

    A new module was added.

---
+++ filesystem/files-diff/src/main/java/org/onebeartoe/development/tools/directory/files/diff/DirectoryFilesDifflService.java
@@ -0,0 +1,11 @@
+
+package org.onebeartoe.development.tools.directory.files.diff;
+
+/**
+ *
+ * @author Roberto Marquez
+ */
+public class DirectoryFilesDifflService 
+{
+    
+}
---
+++ filesystem/files-diff/src/main/java/org/onebeartoe/development/tools/directory/files/diff/DirectoryFilesDifflet.java
@@ -0,0 +1,25 @@
+/*
+ */
+
+package org.onebeartoe.development.tools.directory.files.diff;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Roberto Marquez
+ */
+public class DirectoryFilesDifflet 
+{
+//    @Override
+//    protected AppletService getService()
+//    {
+//        return new DirectoryFilesDifflService();
+//    }
+//    
+//    public static void main(String [] args) throws ParseException, IOException, Exception
+//    {
+//        CommandLineInterfaceApplet app = new DirectoryFilesDifflet();
+//        app.execute(args);
+//    }
+}
