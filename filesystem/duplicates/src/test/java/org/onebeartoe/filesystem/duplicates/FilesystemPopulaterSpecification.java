4b477d9abc95e0e481aa08aa8d5fed0ebd19806c
Roberto Marquez <onebeartoe@gmail.com>
2018-10-26 14:46:15 -0500

    These are changes to correct the TestNG configuration.

--- filesystem/duplicates/src/test/java/org/onebeartoe/filesystem/duplicates/FilesystemPopulaterSpecification.java
+++ filesystem/duplicates/src/test/java/org/onebeartoe/filesystem/duplicates/FilesystemPopulaterSpecification.java
@@ -1,6 +1,11 @@
 
 package org.onebeartoe.filesystem.duplicates;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.PosixFileAttributes;
 import org.apache.commons.cli.Options;
 import org.testng.annotations.Test;
 
@@ -22,5 +27,17 @@ public class FilesystemPopulaterSpecification
         Options buildOptions = implementation.buildOptions();
         
         assert(buildOptions != null);
+    }
+    
+    @Test(groups = {"unit"})
+    public void preVisitdirectory() throws IOException
+    {
+        String dirPath = "target/classes";
+        File f = new File(dirPath);
+        Path dir = f.toPath();
+        
+        BasicFileAttributes attributes = null;
+        
+        implementation.preVisitDirectory(dir, attributes);
     }
 }
