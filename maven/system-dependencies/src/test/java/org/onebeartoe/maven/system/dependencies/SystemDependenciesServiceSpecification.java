fdda15db10dd31ee64d38b78967731afec9316a8
Roberto Marquez <onebeartoe@gmail.com>
2018-11-06 12:27:49 -0600

    More tests were added.

---
+++ maven/system-dependencies/src/test/java/org/onebeartoe/maven/system/dependencies/SystemDependenciesServiceSpecification.java
@@ -0,0 +1,27 @@
+/*
+ */
+package org.onebeartoe.maven.system.dependencies;
+
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Roberto Marquez
+ */
+public class SystemDependenciesServiceSpecification
+{
+    private SystemDependenciesService implementation;
+    
+    public SystemDependenciesServiceSpecification()
+    {
+        implementation = new SystemDependenciesService();
+    }
+    
+    @Test(groups = {"unit"})
+    public void serviceRequest() throws Exception
+    {
+        SystemDependenciesRunProfile runProfile = new SystemDependenciesRunProfile();
+        
+        implementation.serviceRequest(runProfile);
+    }
+}
