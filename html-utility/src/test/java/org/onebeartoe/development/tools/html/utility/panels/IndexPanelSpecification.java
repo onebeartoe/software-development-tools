e150e380342145c51282656dafe6fdab639e3443
Roberto Marquez <onebeartoe@gmail.com>
2018-12-20 10:53:01 -0600

    More unit tests were added.

---
+++ html-utility/src/test/java/org/onebeartoe/development/tools/html/utility/panels/IndexPanelSpecification.java
@@ -0,0 +1,32 @@
+/*
+ */
+package org.onebeartoe.development.tools.html.utility.panels;
+
+import java.awt.event.ActionEvent;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Roberto Marquez
+ */
+public class IndexPanelSpecification
+{
+    IndexPanel implementation;
+    
+    @BeforeTest
+    public void setup()
+    {
+        implementation = new IndexPanel();
+    }
+    
+    @Test
+    public void someMethod()
+    {
+        Object source = new Object();
+        
+        ActionEvent ae = new ActionEvent(source, 0, "no-command");
+        
+        implementation.actionPerformed(ae);
+    }
+}
