2ec3fcf50c54dc6853280a606113748cb8401ad3
Roberto Marquez <onebeartoe@gmail.com>
2020-04-22 10:50:08 -0500

    cleanup

--- filesystem/watcher/command-line/src/main/java/org/onebeartoe/filesystem/watcher/InvalidFileWatcherParamsException.java
+++ filesystem/watcher/command-line/src/main/java/org/onebeartoe/filesystem/watcher/InvalidFileWatcherParamsException.java
@@ -5,14 +5,10 @@ import org.apache.commons.cli.ParseException;
 
 public class InvalidFileWatcherParamsException extends ParseException
 {
-
-    /**
-     *
-     */
     private static final long serialVersionUID = 1L;
 
-    public InvalidFileWatcherParamsException(String message) {
+    public InvalidFileWatcherParamsException(String message) 
+    {
         super(message);
-        // TODO Auto-generated constructor stub
     }
 }
