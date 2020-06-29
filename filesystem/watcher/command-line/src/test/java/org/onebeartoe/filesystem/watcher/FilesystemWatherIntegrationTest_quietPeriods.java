677c9d9d3c1bf95e1c6bc525aa0219b84b634ca0
Roberto Marquez <onebeartoe@gmail.com>
2020-06-29 16:41:16 -0500

    add tests for the quiet period elapsing before the command is executed

--- filesystem/watcher/command-line/src/test/java/org/onebeartoe/filesystem/watcher/FilesystemWatherIntegrationTest_quietPeriods.java
+++ filesystem/watcher/command-line/src/test/java/org/onebeartoe/filesystem/watcher/FilesystemWatherIntegrationTest_quietPeriods.java
@@ -5,13 +5,16 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.attribute.FileTime;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Calendar;
 import java.util.Random;
+import java.util.stream.Stream;
 import org.onebeartoe.io.TextFileWriter;
 import static org.onebeartoe.system.Sleeper.sleepo;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 import org.testng.annotations.Test;
 
@@ -34,32 +37,11 @@ public class FilesystemWatherIntegrationTest_quietPeriods
     /**
      * US01AC06
      */    
-     public void initialQuitPeriodIsIgnored() throws IOException 
+    public void initialQuitPeriodIsIgnored() throws IOException 
     {
         String testName = "initialQuitPeriodIsIgnored";
-                
-        String immediateEcho = "This-is-append-text-IMMIDIATELY-after-initialization.";
         
-        TestDirectoryWatcherProfile profile = watchProfile(testName, immediateEcho);
-
-        DirectoryWatcher implementation = new DirectoryWatcher(profile);
-            
-        implementation.processEvents();
-        
-        File touchFile = new File(profile.directory.toFile(), "some-file.text");
-        
-        touch(touchFile);    
-        
-        WatcherItem item = profile.watchItems.get(0);
-        
-        long sleepoMillis = item.quietPeriod.toMillis() / 4;  // wait a quarter of quite period 
-        
-        sleepo(sleepoMillis);
-                
-        // make sure the command goes off immediately after initialization 
-        //      (no inital quiet period)
-        File infile = new File(item.outpath);
-        assertFileContains(infile, immediateEcho);
+        TestDirectoryWatcherProfile profile = assertInitialQuitPeriodIsIgnored(testName);
     }
 
 //TODO: this test     
@@ -106,17 +88,51 @@ String echoContent = null;
         throw new Exception("implment");
     }
 
-    @Test
-//TODO: this test            
+    @Test//(testName = "US01AC10")
     /**
-     * 
-     * 
      * US01AC10 
      */
     public void filesystemModificationOccursBeforeTheQuitePeriod() throws Exception
     {
-//TODO: implement        
-        throw new Exception("implment");
+        String testName = "US01AC10";
+        
+        TestDirectoryWatcherProfile profile = assertInitialQuitPeriodIsIgnored(testName);
+        
+        WatcherItem item = profile.watchItems.get(0);
+                        
+        // wait half the quiet perid time
+        long halfQuietTimeSleepoMillis = item.quietPeriod.toMillis() / 2;
+
+        sleepo(halfQuietTimeSleepoMillis);
+        
+        // touch a file under watch directory
+        File touchFile = new File(profile.directory.toFile(), "before-mod.text");
+        
+        touch(touchFile);        
+        
+        // waite one second
+        sleepo( Duration.ofSeconds(1).toMillis() );
+        
+        // assert the watcher outfile file only contains one entry from initial (quiet period was restared)
+        String watchOutpath = profile.watchItems.get(0).outpath;
+        
+        Path outpath = Paths.get(watchOutpath);
+        
+        Stream<String> initialStream = Files.lines(outpath);
+        
+        Object[] initailArray = initialStream.toArray();
+        
+        assertEquals(initailArray.length, 1);
+
+        // waite the quiet period plus one second
+        sleepo(item.quietPeriod.toMillis() + 1000);
+        
+        // assert the watcher out file now contains two entrys 
+        Stream<String> postStream = Files.lines( touchFile.toPath() );
+        
+        Object[] postArray = postStream.toArray();
+        
+        assertEquals(postArray.length, 2);
     }
 
     private void append(File fileToWatch, String immediateEcho) throws IOException
@@ -212,5 +228,34 @@ String echoContent = null;
         profile.recursive = recursive;
 
         return profile;        
+    }
+
+    private TestDirectoryWatcherProfile assertInitialQuitPeriodIsIgnored(String testName) throws IOException
+    {
+//TODO: change the value to something more generic for all the tests        
+        String immediateEcho = "This-is-append-text-IMMIDIATELY-after-initialization.";
+        
+        TestDirectoryWatcherProfile profile = watchProfile(testName, immediateEcho);
+
+        DirectoryWatcher implementation = new DirectoryWatcher(profile);
+            
+        implementation.processEvents();
+        
+        File touchFile = new File(profile.directory.toFile(), "some-file.text");
+        
+        touch(touchFile);
+        
+        WatcherItem item = profile.watchItems.get(0);
+        
+        long sleepoMillis = item.quietPeriod.toMillis() / 4;  // wait a quarter of quite period 
+        
+        sleepo(sleepoMillis);
+                
+        // make sure the command goes off immediately after initialization 
+        //      (no inital quiet period)
+        File infile = new File(item.outpath);
+        assertFileContains(infile, immediateEcho);
+        
+        return profile;
     }
 }
--- filesystem/watcher/command-line/src/test/resources/integration/echo.sh
+++ filesystem/watcher/command-line/src/test/resources/integration/echo.sh
@@ -1,4 +1,2 @@
 
-echo hi
-
 echo "I am a flower, $1" >> $2
