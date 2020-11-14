7640796916b9a5b8594470d38a9f7bba301e5b7a
Roberto Marquez <onebeartoe@gmail.com>
2020-11-14 05:14:47 -0700

    add awk documentation

--- filesystem/watcher/command-line/demos/word-count/wordcount.sh
+++ filesystem/watcher/command-line/demos/word-count/wordcount.sh
@@ -1,6 +1,10 @@
 
 # populate the word count file
+# find all text files
+#                       count the words in the text files
+#                                     remove the line with total words
+#                                                write the output to disk
 find . -name "*.text" | xargs wc -w | sed '$d' > wc.out
 
-# parse the word count file
+# parse the word count output file
 awk -f report.awk wc.out
