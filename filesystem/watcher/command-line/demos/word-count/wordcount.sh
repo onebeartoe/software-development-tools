7a75c96e5902d1ef228f110033408576ac37584a
Roberto Marquez <onebeartoe@gmail.com>
2020-11-13 17:48:50 -0700

    cleanup

--- filesystem/watcher/command-line/demos/word-count/wordcount.sh
+++ filesystem/watcher/command-line/demos/word-count/wordcount.sh
@@ -1,8 +1,6 @@
 
 # populate the word count file
 find . -name "*.text" | xargs wc -w | sed '$d' > wc.out
-#find . -name "*.text" | xargs wc -l | sed '$d' > wc.out
-#wc -l *.text > wc.out
 
 # parse the word count file
 awk '{if($1 > 10) {print "over: ", $0; print "something else";} else print "good: " , $0;}' wc.out
