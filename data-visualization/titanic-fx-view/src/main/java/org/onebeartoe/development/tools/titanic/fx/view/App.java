7af91c98a4fd5608a084b3e6613b65107e8ae24a
Roberto Marquez <onebeartoe@gmail.com>
2023-10-13 10:22:43 -0500

    debug

--- data-visualization/titanic-fx-view/src/main/java/org/onebeartoe/development/tools/titanic/fx/view/App.java
+++ data-visualization/titanic-fx-view/src/main/java/org/onebeartoe/development/tools/titanic/fx/view/App.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+//import org.onebeartoe.development.tools.titanic.
 import org.onebeartoe.development.tools.titanic.TitanicDataSource;
 
 
