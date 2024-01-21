5a26cadc34cb8478f4e43f1cba4cfc5b56c33a07
Roberto Marquez <onebeartoe@gmail.com>
2024-01-21 11:46:19 -0600

    add a histogram

--- data-visualization/titanic-statistics/src/main/R/titanic.R
+++ data-visualization/titanic-statistics/src/main/R/titanic.R
@@ -1,7 +1,14 @@
 
 install.packages("tidyverse")
 
+
+library(tidyverse)
+
+
 titanicData <- read.csv("titanic-passenger-list.csv")
 
 dim(titanicData)
 
+ggplot(data=titanicData, aes(x=age)) +
+  geom_histogram(fill="steelblue", color="black") +
+  ggtitle("Histogram of Age Values")
