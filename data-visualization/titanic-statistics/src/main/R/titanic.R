bbe29039c17423d21bc808760468fe5ff1ca701d
Roberto Marquez <onebeartoe@gmail.com>
2024-01-21 12:20:22 -0600

    add means

--- data-visualization/titanic-statistics/src/main/R/titanic.R
+++ data-visualization/titanic-statistics/src/main/R/titanic.R
@@ -9,6 +9,8 @@ titanicData <- read.csv("titanic-passenger-list.csv")
 
 dim(titanicData)
 
+summary(titanicData)
+
 ggplot(data=titanicData, aes(x=age)) +
   geom_histogram(fill="steelblue", color="black") +
   ggtitle("Histogram of Age Values")
