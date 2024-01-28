97ce7e489d665ec25e9c0e3c1640b4535d56a371
Roberto Marquez <onebeartoe@gmail.com>
2024-01-28 17:45:51 -0600

    try graphing based on a different column

--- data-visualization/titanic-statistics/src/main/R/titanic.R
+++ data-visualization/titanic-statistics/src/main/R/titanic.R
@@ -15,3 +15,8 @@ ggplot(data=titanicData, aes(x=age)) +
   geom_histogram(fill="steelblue", color="black") +
   ggtitle("Histogram of Age Values")
 
+# I could not get this next graph to work
+#ggplot(data=titanicData, aes(x=sex)) +
+#  geom_histogram(fill="steelblue", color="black") +
+#  ggtitle("Histogram of Sex Values")
+
